28524e8f841b609fe6b6b0577f4efdb60988aba7
hachan140 <havu14102@gmail.com>
2023-10-09 23:45:10 +0700

    add repo

---
+++ CinemaSystem/CinemaSystemLibrary/Repository/Interface/IRoomRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaSystemLibrary.Repository.Interface
+{
+    internal interface IRoomRepository
+    {
+    }
+}
